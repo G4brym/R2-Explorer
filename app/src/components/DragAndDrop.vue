360f51063629ddce8f137c278a20cd33002839c1
Gabriel Massadas <gmassadas@cloudflare.com>
2023-01-14 12:15:21 +0000

    Fix drag and drop turning on and off repeatedly

--- app/src/components/DragAndDrop.vue
+++ app/src/components/DragAndDrop.vue
@@ -1,12 +1,14 @@
 <template>
   <div
-    :class="{ 'upload-box': isHover }"
+    ref="dragContainer"
+    class="upload-box"
+    :class="{ 'active': isHover || false }"
     @dragover.prevent="dragover"
     @dragleave.prevent="dragleave"
     @drop.prevent="drop"
   >
     <slot></slot>
-    <div v-if="isHover" class="drop-files">
+    <div class="drop-files">
       <div class="box">
           <h3>Drop files to upload</h3>
         <span class="font-28"><i class="bi bi-cloud-upload-fill"></i></span>
@@ -25,7 +27,13 @@ export default {
   data: function () {
     return {
       isHover: false,
-      filelist: [] // Store our uploaded files
+      filelist: [], // Store our uploaded files
+      dragContainer: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+      }
     }
   },
   methods: {
@@ -33,10 +41,25 @@ export default {
       this.$refs.uploader.click()
     },
     dragover (event) {
+      const coordinates = this.$refs.dragContainer.getBoundingClientRect()
+      this.dragContainer = {
+        top: coordinates.top,
+        bottom: coordinates.bottom,
+        left: coordinates.left,
+        right: coordinates.right
+      }
+
       this.isHover = true
     },
     dragleave (event) {
-      this.isHover = false
+      if (
+        event.clientX < this.dragContainer.left ||
+        event.clientX > this.dragContainer.right ||
+        event.clientY > this.dragContainer.bottom ||
+        event.clientY < this.dragContainer.top
+      ) {
+        this.isHover = false
+      }
     },
     inputFiles (event) {
       this.uploadFiles(event.target.files)
@@ -64,6 +87,7 @@ export default {
       self.$store.dispatch('refreshObjects')
     },
     drop (event) {
+      console.log(event.dataTransfer)
       event.preventDefault()
       // console.log('drop', event.dataTransfer.files)
       this.uploadFiles(event.dataTransfer.files)
