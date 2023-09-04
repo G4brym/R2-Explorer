d0f502e5798ad7dce82bb1bb49d81c58dcbcc7c8
Zehuan Li <22104836+zehuanli@users.noreply.github.com>
2023-09-04 00:23:12 -0400

    Copying from receiveEmail.ts and EmailDetails.vue

--- packages/dashboard/src/components/preview/EmailViewer.vue
+++ packages/dashboard/src/components/preview/EmailViewer.vue
@@ -1,16 +1,55 @@
 <template>
-  <div>
-    <div v-if="emailData.sender">
-      <strong>Sender:</strong> {{ emailData.sender }}
+  <template v-if="emailData">
+
+    <h5 class="font-18">{{ emailData.subject }}</h5>
+
+    <hr/>
+
+    <div class="d-flex align-items-start mb-2 mt-1">
+      <div class="w-100">
+        <small class="float-end">{{ emailData.date }}</small>
+        <small class="text-muted">From: {{ emailData.from?.name }} &lt;{{ emailData.from?.address }}&gt;</small>
+      </div>
+      
     </div>
-    <div v-if="emailData.receiver">
-      <strong>Receiver:</strong> {{ emailData.receiver }}
+    <div class="w-100">
+        <small class="text-muted">To: 
+          <template v-for="recipient of emailData.to">
+            {{ recipient.name }} &lt;{{ recipient.address }}&gt;&semi;
+          </template>
+        </small>
     </div>
-    <div v-if="emailData.content">
-      <strong>Content:</strong>
-      <div v-html="emailData.content"></div>
+
+    <br/>
+
+    <template v-if="emailData.text">
+      <div class="overflow-auto" v-html="emailData.text.replaceAll('\n', '<br>')"></div>
+    </template>
+
+    <template v-else>
+      <div class="overflow-auto">[Empty text]</div>
+    </template>
+
+    <hr/>
+
+    <template v-if="emailData.html">
+      <h5 class="mb-3">This email contains HTML content</h5>
+    </template>
+
+    <template v-if="emailData.attachments?.length > 0">
+      <h5 class="mb-3">This email has {{ emailData.attachments.length }} attachment(s):</h5>
+      <div class="w-100" v-for="attachment of emailData.attachments" :key="attachment.filename">
+        <small class="text-muted">{{ attachment.filename }}</small>
+      </div>
+    </template>
+  </template>
+
+  <template v-else>
+    <div class="text-center">
+      <div class="lds-dual-ring"></div>
     </div>
-  </div>
+  </template>
+
 </template>
 
 <script>
@@ -27,25 +66,16 @@ export default {
     async parseEmail() {
       try {
         const parser = new PostalMime.default();
-        const parsedEmailData = await parser.parse(this.filedata);
+        const parsedEmail = await parser.parse(this.filedata);
 
-        // Extract sender, receiver, and content
-        const sender = parsedEmailData.from?.address;
-        const receiver = parsedEmailData.to?.address;
-        const content = parsedEmailData.text;
-
-        this.emailData = {
-          sender,
-          receiver,
-          content,
-        };
+        this.emailData = parsedEmail;
       } catch (error) {
         console.error('Error parsing email data:', error);
       }
     }
   },
-  mounted() {
-    this.parseEmail();
+  async mounted() {
+    await this.parseEmail();
   }
 }
 </script>
