66f509566e5f2d9ca1f81600ac3a82b81ca380c8
Gabriel Massadas <gmassadas@cloudflare.com>
2024-12-01 20:33:08 +0000

    Add cloudflare create configurations

---
+++ c3.ts
@@ -0,0 +1,7 @@
+export default {
+  configVersion: 1,
+  id: "r2explorer",
+  displayName: "R2Explorer interface for your Cloudflare R2 Buckets!",
+  platform: "workers",
+  copyFiles: { path: './template' }
+};
---
+++ template/src/index.ts
@@ -0,0 +1,9 @@
+import { R2Explorer } from 'r2-explorer';
+
+export default R2Explorer({
+  readonly: true,
+  // basicAuth: {
+  //   username: 'username',
+  //   password: 'password'
+  // }
+});
