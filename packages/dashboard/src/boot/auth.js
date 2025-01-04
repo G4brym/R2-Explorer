a74fde5af683cac6641671dc768c07223b968ece
Gabriel Massadas <5445926+G4brym@users.noreply.github.com>
2025-01-04 12:59:00 +0000

    Improve error messages (#77)

--- packages/dashboard/src/boot/auth.js
+++ packages/dashboard/src/boot/auth.js
@@ -2,14 +2,21 @@ import { boot } from "quasar/wrappers";
 import { useAuthStore } from "stores/auth-store";
 import { useMainStore } from "stores/main-store";
 
-export default boot(async ({ router, store }) => {
+export default boot(async ({ app, router, store }) => {
 	// Check if theres any auth token stored, if there is, try to fetch or redirect
 	const authStore = useAuthStore(store);
-	const authResp = await authStore.CheckLoginInStorage(router);
+	const authResp = await authStore.CheckLoginInStorage(
+		router,
+		app.config.globalProperties.$q,
+	);
 
 	if (authResp === false) {
 		// No auth token stored, try to fetch without auth or redirect
 		const mainStore = useMainStore(store);
-		await mainStore.loadServerConfigs(router, true);
+		await mainStore.loadServerConfigs(
+			router,
+			app.config.globalProperties.$q,
+			true,
+		);
 	}
 });
