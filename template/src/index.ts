d13a1aa12d2fe9121b2d10cb9e71aee2425b0dd0
beccangruenberg <git@beccan.gruenbergs.net>
2025-05-05 12:39:26 -0400

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,6 +3,7 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.dev/getting-started/configuration/
 export default R2Explorer({
 	readonly: true,
+	cfAccessTeamName: 'gruenbergs',
 	// basicAuth: {
 	//   username: 'username',
 	//   password: 'password'
