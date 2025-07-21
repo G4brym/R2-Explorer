57c0bfcb8e9377742e135fea5e394dd47b23271e
Skye <46286597+Skye-31@users.noreply.github.com>
2025-07-22 00:54:44 +0100

    Only show bucket picker if there are several buckets

--- packages/dashboard/src/components/main/Topbar.vue
+++ packages/dashboard/src/components/main/Topbar.vue
@@ -8,18 +8,23 @@
     R2-Explorer
   </q-toolbar-title>
   <q-space />
-  <div>
+  <div v-if="mainStore.buckets.length > 1">
     <bucket-picker/>
   </div>
 </template>
 
 <script>
 import BucketPicker from "components/main/BucketPicker.vue";
+import { useMainStore } from "stores/main-store";
 import { defineComponent } from "vue";
 
 export default defineComponent({
 	name: "TopBar",
 	emits: ["toggle"],
 	components: { BucketPicker },
+	setup() {
+		const mainStore = useMainStore();
+		return { mainStore };
+	},
 });
 </script>
