7b84235fcf7e69cecfbdbdd596f233dda6598117
Gabriel Massadas <gmassadas@cloudflare.com>
2026-01-12 19:21:57 +0000

    Fix #114

--- template/src/index.ts
+++ template/src/index.ts
@@ -4,7 +4,7 @@ import { R2Explorer } from "r2-explorer";
 export default R2Explorer({
 	readonly: true,
 	// basicAuth: {
-	//   username: 'username',
-	//   password: 'password'
-	// }
+	// 	username: "username",
+	// 	password: "password",
+	// },
 });
